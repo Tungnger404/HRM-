57cbf0bc1425628a9da7f8c11aaba966f75b69e2
Lanamne <lebanhatnam996@gmail.com>
2026-02-05 13:26:26 +0700

    Screen 2: Recruitment Request List (HR)

--- src/main/java/com/example/hrm/repository/RecruitmentRequestRepository.java
+++ src/main/java/com/example/hrm/repository/RecruitmentRequestRepository.java
@@ -2,12 +2,25 @@ package com.example.hrm.repository;
 
 import com.example.hrm.entity.Employee;
 import com.example.hrm.entity.RecruitmentRequest;
+import com.example.hrm.entity.RecruitmentRequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface RecruitmentRequestRepository
+public interface    RecruitmentRequestRepository
         extends JpaRepository<RecruitmentRequest, Integer> {
 
     List<RecruitmentRequest> findByCreatedBy(Employee employee);
+    @Query("""
+        SELECT r
+        FROM RecruitmentRequest r
+        JOIN FETCH r.department
+        JOIN FETCH r.jobPosition
+        WHERE r.status <> :status
+    """)
+    List<RecruitmentRequest> findForHR(
+            @Param("status") RecruitmentRequestStatus status
+    );
 }
