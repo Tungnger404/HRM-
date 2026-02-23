e68961d1ec63f4d6c2a96739398c4525df680d42
pcuongmc72 <mcphucuongng72@gmail.com>
2026-02-23 22:56:32 +0700

    upload request...

---
+++ src/main/java/com/example/hrm/repository/LeaveOrOtRequestRepository.java
@@ -0,0 +1,8 @@
+package com.example.hrm.repository;
+
+import com.example.hrm.entity.LeaveOrOtRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeaveOrOtRequestRepository
+        extends JpaRepository<LeaveOrOtRequest, Integer> {
+}
